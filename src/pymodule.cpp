03b3f5e6dd36265222697a333ce1266c51e3a65f
Hongze Xia <hongze.x@gmail.com>
2019-09-01 20:49:22 +1000

    rm wrapper class

--- src/pymodule.cpp
+++ src/pymodule.cpp
@@ -1,12 +1,15 @@
+#include <leptonica/allheaders.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <pysseract.h>
+#include <tesseract/baseapi.h>
+#include <tesseract/genericvector.h>
 #include <sstream>
 
 namespace py = pybind11;
 using tesseract::PageIteratorLevel;
 using tesseract::PageSegMode;
 using tesseract::ResultIterator;
+using tesseract::TessBaseAPI;
 
 PYBIND11_MODULE(pysseract, m) {
     m.doc() = R"pbdoc(
@@ -20,48 +23,88 @@ PYBIND11_MODULE(pysseract, m) {
            availableLanguages
     )pbdoc";
 
-    m.def("apiVersion", &tesseract::TessBaseAPI::Version, R"pbdoc(
-        Tesseract API version as seen in the library
-    )pbdoc");
-    m.def("availableLanguages", &availableLanguages, R"pbdoc(
-        return a list of available languages from TESSDATA_PREFIX
-    )pbdoc");
-    m.def("defaultDataPath", &defaultDataPath, R"pbdoc(
-        return TESSDATA_PREFIX
-    )pbdoc");
+    m.def("apiVersion", &tesseract::TessBaseAPI::Version, "Tesseract API version as seen in the library");
+    m.def("availableLanguages",
+          []() {
+              tesseract::TessBaseAPI api;
+              api.Init(nullptr, nullptr);
+              GenericVector<STRING> glangs;
+              api.GetAvailableLanguagesAsVector(&glangs);
+              std::vector<std::string> langs(glangs.size());
+              for (int i = 0; i < glangs.size(); i++) {
+                  langs[i] = glangs[i].c_str();
+              }
+              return langs;
+          },
+          "return a list of available languages from TESSDATA_PREFIX");
+    m.def("defaultDataPath",
+          []() {
+              tesseract::TessBaseAPI api;
+              api.Init(nullptr, nullptr);
+              return api.GetDatapath();
+          },
+          "return TESSDATA_PREFIX");
 
-    py::class_<Pysseract>(m, "Pysseract", R"pbdoc(
+    py::class_<TessBaseAPI>(m, "Pysseract", R"pbdoc(
         Pysseract
         ---------------------
         main class to interact with Tesseract API
     )pbdoc")
-        .def(py::init<const char *, const char *>(), py::arg("datapath"), py::arg("language"))
-        .def(py::init<>())
-        .def("Clear", &Pysseract::Clear, R"pbdoc(
-            Free up recognition results and any stored image data, without actually
-            freeing any recognition data that would be time-consuming to reload.
-            Afterwards, you must call SetImage or TesseractRect before doing
-            any Recognize or Get* operation.
-        )pbdoc")
-        .def_property_readonly("dataPath", &Pysseract::GetDataPath)
-        .def_property("pageSegMode", &Pysseract::GetPageSegMode, &Pysseract::SetPageSegMode)
-        .def_property_readonly("utf8Text", &Pysseract::GetUTF8Text)
-        .def_property_readonly("unlvText", &Pysseract::GetUNLVText)
-        .def("GetHOCRText", &Pysseract::GetHOCRText, py::arg("pagenum"))
-        .def("GetTSVText", &Pysseract::GetTSVText, py::arg("pagenum"))
-        .def("GetAltoText", &Pysseract::GetAltoText, py::arg("pagenum"))
-        .def("GetLSTMBoxText", &Pysseract::GetLSTMBoxText, py::arg("pagenum"))
-        .def("GetWordStrBoxText", &Pysseract::GetWordStrBoxText, py::arg("pagenum"))
-        .def("GetOsdText", &Pysseract::GetOsdText, py::arg("pagenum"))
-        .def("GetIterator", &Pysseract::GetIterator)
-        .def("SetSourceResolution", &Pysseract::SetSourceResolution, py::arg("ppi"))
-        .def("SetImageFromPath", &Pysseract::SetImageFromPath, py::arg("imgpath"))
-        .def("SetImageFromBytes", &Pysseract::SetImageFromBytes, py::arg("bytes"))
-        .def("SetVariable", &Pysseract::SetVariable, py::arg(", name"), py::arg("value"),
-             R"pbdoc(Note: Must be called after Init(). Only works for non-init variables.)pbdoc")
-        .def("SetRectangle", &Pysseract::SetRectangle, py::arg("left"), py::arg("top"), py::arg("width"),
-             py::arg("height"),
-             R"pbdoc(Restrict recognition to a sub-rectangle of the image. Call after SetImage.)pbdoc");
+        .def(py::init([](const char *datapath, const char *language) {
+                 TessBaseAPI *api = new (TessBaseAPI);
+                 api->Init(datapath, language);
+                 return std::unique_ptr<TessBaseAPI>(api);
+             }),
+             py::arg("datapath"), py::arg("language"))
+        .def(py::init([]() {
+            TessBaseAPI *api = new (TessBaseAPI);
+            api->Init(nullptr, nullptr);
+            return std::unique_ptr<TessBaseAPI>(api);
+        }))
+        .def("Clear", &TessBaseAPI::Clear, R"pbdoc(
+        Free up recognition results and any stored image data, without actually
+        freeing any recognition data that would be time-consuming to reload.
+        Afterwards, you must call SetImage or TesseractRect before doing
+        any Recognize or Get* operation.
+    )pbdoc")
+        .def("End", &TessBaseAPI::End,
+             "Close down tesseract and free up all memory, after which the instance should not be reused.")
+        .def("__enter__", [](TessBaseAPI &api) { return &api; }, "for with statement")
+        .def("__exit__",
+             [](TessBaseAPI &api, py::object exc_type, py::object exc_value, py::object traceback) { api.End(); },
+             "for with statement, after exitting with, the instance is not reusable.")
+        .def_property_readonly("dataPath", &TessBaseAPI::GetDatapath)
+        .def_property("pageSegMode", &TessBaseAPI::GetPageSegMode, &TessBaseAPI::SetPageSegMode)
+        .def_property_readonly("utf8Text", &TessBaseAPI::GetUTF8Text)
+        .def_property_readonly("unlvText", &TessBaseAPI::GetUNLVText)
+        .def("GetHOCRText", (char *(TessBaseAPI::*)(int)) & TessBaseAPI::GetHOCRText, py::arg("pagenum"))
+        .def("GetTSVText", &TessBaseAPI::GetTSVText, py::arg("pagenum"))
+        .def("GetAltoText", (char *(TessBaseAPI::*)(int)) & TessBaseAPI::GetAltoText, py::arg("pagenum"))
+        .def("GetLSTMBoxText", &TessBaseAPI::GetLSTMBoxText, py::arg("pagenum"))
+        .def("GetWordStrBoxText", &TessBaseAPI::GetWordStrBoxText, py::arg("pagenum"))
+        .def("GetOsdText", &TessBaseAPI::GetOsdText, py::arg("pagenum"))
+        .def("GetIterator",
+             [](TessBaseAPI &api) {
+                 api.Recognize(nullptr);
+                 return api.GetIterator();
+             })
+        .def("SetSourceResolution", &TessBaseAPI::SetSourceResolution, py::arg("ppi"))
+        .def("SetImageFromPath",
+             [](TessBaseAPI &api, const char *imgpath) {
+                 Pix *image = pixRead(imgpath);
+                 api.SetImage(image);
+             },
+             py::arg("imgpath"))
+        .def("SetImageFromBytes",
+             [](TessBaseAPI &api, const std::string &bytes) {
+                 Pix *image = pixReadMem((unsigned char *)bytes.data(), bytes.size());
+                 api.SetImage(image);
+             },
+             py::arg("bytes"))
+        .def("SetVariable", &TessBaseAPI::SetVariable, py::arg("name"), py::arg("value"),
+             "Note: Must be called after Init(). Only works for non-init variables.")
+        .def("SetRectangle", &TessBaseAPI::SetRectangle, py::arg("left"), py::arg("top"), py::arg("width"),
+             py::arg("height"), "Restrict recognition to a sub-rectangle of the image. Call after SetImage.");
 
     py::enum_<PageIteratorLevel>(m, "PageIteratorLevel")
         .value("BLOCK", PageIteratorLevel::RIL_BLOCK)
