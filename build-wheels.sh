a168b624c028231ddd526bd07a25cce215c85275
StephenHogg <StephenHogg@users.noreply.github.com>
2019-11-24 18:24:15 +1100

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -53,7 +53,7 @@ yum -y install libpng-devel libtiff-devel libjpeg-devel
 #source /opt/rh/devtoolset-7/enable
 cd /usr/src/
 wget http://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2019.01.06.tar.xz
-tar xvvfJ autoconf-archive-2019.01.06.tar.xz
+tar xvvfj autoconf-archive-2019.01.06.tar.xz
 cd autoconf-archive-2019.01.06/
 ./configure --prefix=/usr
 make
