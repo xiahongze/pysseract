563e718f8cc17f9daef2b0173714e8594d92f98f
StephenHogg <StephenHogg@users.noreply.github.com>
2019-11-25 00:33:19 +1100

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -46,11 +46,12 @@ make install
 export PYHOME=/home
 cd ${PYHOME}
 
-/opt/python/cp37-cp37m/bin/pip install twine cmake
+/opt/python/cp37-cp37m/bin/pip install twine cmake 
 ln -s /opt/python/cp37-cp37m/bin/cmake /usr/bin/cmake
 
 # Compile wheels
 for PYBIN in /opt/python/cp37*/bin; do
+    "${PYBIN}/pip" install pybind11
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
     "${PYBIN}/python" /io/setup.py sdist -d /io/wheelhouse/
 done
