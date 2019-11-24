45a58282258b15ca23dc0b3c09f7572c7c9c34b6
StephenHogg <StephenHogg@users.noreply.github.com>
2019-11-25 00:49:56 +1100

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -25,10 +25,10 @@ yum -y install tesseract-langpack-eng
 cd /usr/src
 wget --quiet --no-check-certificate http://leptonica.org/source/leptonica-1.77.0.tar.gz
 tar xfz leptonica-1.77.0.tar.gz
-#cd leptonica-1.77.0/
-#./configure --prefix=/usr/local/
-#make
-#make install
+cd leptonica-1.77.0/
+./configure --prefix=/usr/local/
+make
+make install
 #cd /usr/src/
 wget --quiet https://github.com/tesseract-ocr/tesseract/archive/4.0.0.tar.gz -O tesseract-4.0.0.tar.gz
 tar xvvfz tesseract-4.0.0.tar.gz
@@ -41,8 +41,8 @@ tar xvvfz tesseract-4.0.0.tar.gz
 #./configure --prefix=/usr/local/ --with-extra-libraries=/usr/local/lib/
 #make install
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-mv /usr/src/leptonica-1.77.0/src /usr/src/leptonica-1.77.0/leptonica
-ln -s /usr/src/leptonica-1.77.0/leptonica /usr/local/include
+#mv /usr/src/leptonica-1.77.0/src /usr/src/leptonica-1.77.0/leptonica
+#ln -s /usr/src/leptonica-1.77.0/leptonica /usr/local/include
 ln -s /usr/src/tesseract-4.0.0/include/tesseract /usr/local/include
 
 # From here on, the script is building and testing our package
