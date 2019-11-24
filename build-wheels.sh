33a9f6caaf981e46fb5a4515cd0d52c5a80e45f3
StephenHogg <StephenHogg@users.noreply.github.com>
2019-11-24 20:16:51 +1100

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -30,7 +30,7 @@ cd tesseract-4.1.0
 curl -L https://github.com/tesseract-ocr/tessdata_fast/blob/master/eng.traineddata >> ./eng.traineddata
 curl -L https://github.com/tesseract-ocr/tessdata_fast/blob/master/osd.traineddata >> ./osd.traineddata
 cp   eng.traineddata osd.traineddata ./tessdata/
-export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+export PKG_CONFIG_PATH=/usr/lib/pkgconfig
 ./autogen.sh
 ./configure --prefix=/usr/local/ --with-extra-libraries=/usr/local/lib/
 make install
