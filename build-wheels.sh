af42a47388b59e70120f51f60e7b57a83d76222a
StephenHogg <StephenHogg@users.noreply.github.com>
2019-11-24 23:32:58 +1100

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -10,9 +10,10 @@ yum -y groupinstall "development tools"
 yum -y install libpng-devel libtiff-devel libjpeg-devel
 yum -y install centos-release-scl
 yum -y install devtoolset-7-gcc-c++
+source /opt/rh/devtoolset-7/enable
 cd /usr/src/
 wget --quiet http://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2019.01.06.tar.xz
-tar xvvfJ autoconf-archive-2019.01.06.tar.xz
+tar xfJ autoconf-archive-2019.01.06.tar.xz
 cd autoconf-archive-2019.01.06/
 ./configure --prefix=/usr
 make
