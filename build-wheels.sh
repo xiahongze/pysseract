207dcd4699b246cc557535861b3f9199976dcf13
StephenHogg <StephenHogg@users.noreply.github.com>
2019-11-24 19:41:38 +1100

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -4,7 +4,7 @@ yum -y install wget xz
 yum -y install epel-release
 
 #wget --no-check-certificate https://github.com/tesseract-ocr/tesseract/archive/4.1.0.tar.gz -O /usr/src/tesseract-4.1.0.tar.gz
-curl -L https://github.com/tesseract-ocr/tesseract/archive/4.1.0.tar.gz -O /usr/src/tesseract-4.1.0.tar.gz
+curl -L https://github.com/tesseract-ocr/tesseract/archive/4.1.0.tar.gz >> /usr/src/tesseract-4.1.0.tar.gz
 
 # From https://www.tekovic.com/installing-tesseract-ocr-40-on-centos-6
 # This code installs tesseract 4 on CentOS 6
