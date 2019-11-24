967508fde5ed65224bfbc8e3b9e153adcddb579f
StephenHogg <StephenHogg@users.noreply.github.com>
2019-11-24 18:03:01 +1100

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e -x
 yum -y install wget
+yum -y install epel-release
 
 MACHINE_TYPE=`uname -m`
 SERVER=http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/
@@ -24,19 +25,19 @@ GLIBC32_4=glibc-headers-2.17-55.el6.i686.rpm
 GLIBC32_5=glibc-static-2.17-55.el6.i686.rpm
 # update glibc and download conda
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-    wget --quiet $SERVER64/$GLIBC64_1
-    wget --quiet $SERVER64/$GLIBC64_2
-    wget --quiet $SERVER64/$GLIBC64_3
-    wget --quiet $SERVER64/$GLIBC64_4
-    wget --quiet $SERVER64/$GLIBC64_5
+#    wget --quiet $SERVER64/$GLIBC64_1
+#    wget --quiet $SERVER64/$GLIBC64_2
+#    wget --quiet $SERVER64/$GLIBC64_3
+#    wget --quiet $SERVER64/$GLIBC64_4
+#    wget --quiet $SERVER64/$GLIBC64_5
     # yum -y install $GLIBC64_1 $GLIBC64_2 $GLIBC64_3 $GLIBC64_4 $GLIBC64_5
     wget --quiet --no-check-certificate https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh
 else
-    wget --quiet $SERVER32/$GLIBC32_1
-    wget --quiet $SERVER32/$GLIBC32_2
-    wget --quiet $SERVER32/$GLIBC32_3
-    wget --quiet $SERVER32/$GLIBC32_4
-    wget --quiet $SERVER32/$GLIBC32_5
+#    wget --quiet $SERVER32/$GLIBC32_1
+#    wget --quiet $SERVER32/$GLIBC32_2
+#    wget --quiet $SERVER32/$GLIBC32_3
+#    wget --quiet $SERVER32/$GLIBC32_4
+#    wget --quiet $SERVER32/$GLIBC32_5
     # yum -y install $GLIBC32_1 $GLIBC32_2 $GLIBC32_3 $GLIBC32_4 $GLIBC32_5
     wget --quiet --no-check-certificate https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86.sh -O ./miniconda.sh
 fi
@@ -45,37 +46,37 @@ fi
 # From https://www.tekovic.com/installing-tesseract-ocr-40-on-centos-6
 # This code installs tesseract 4 on CentOS 6
 export PATH="$PATH:/usr/local/bin"
-#yum -y groupinstall "development tools"
-#yum -y install libpng-devel libtiff-devel libjpeg-devel
-#yum -y install centos-release-scl
-#yum -y install devtoolset-7-gcc-c++
-#source /opt/rh/devtoolset-7/enable
-#cd /usr/src/
-#wget http://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2019.01.06.tar.xz
-#tar xvvfJ autoconf-archive-2019.01.06.tar.xz
-#cd autoconf-archive-2019.01.06/
-#./configure --prefix=/usr
-#make
-#make install
-#cd /usr/src/
-#wget http://leptonica.org/source/leptonica-1.77.0.tar.gz
-#tar xvvfz leptonica-1.77.0.tar.gz
-#cd leptonica-1.77.0/
-#./configure --prefix=/usr/local/
-#make
-#make install
-#cd /usr/src/
-#wget https://github.com/tesseract-ocr/tesseract/archive/4.1.0.tar.gz -O tesseract-4.1.0.tar.gz
-#tar xvvfz tesseract-4.1.0.tar.gz
-#cd tesseract-4.1.0
-#wget -nc https://github.com/tesseract-ocr/tessdata_fast/blob/master/eng.traineddata
-#wget -nc https://github.com/tesseract-ocr/tessdata_fast/blob/master/osd.traineddata
-#cp   eng.traineddata osd.traineddata ./tessdata/
-#export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
-#./autogen.sh
-#./configure --prefix=/usr/local/ --with-extra-libraries=/usr/local/lib/
-#make install
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+yum -y groupinstall "development tools"
+yum -y install libpng-devel libtiff-devel libjpeg-devel
+yum -y install centos-release-scl
+yum -y install devtoolset-7-gcc-c++
+source /opt/rh/devtoolset-7/enable
+cd /usr/src/
+wget http://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2019.01.06.tar.xz
+tar xvvfJ autoconf-archive-2019.01.06.tar.xz
+cd autoconf-archive-2019.01.06/
+./configure --prefix=/usr
+make
+make install
+cd /usr/src/
+wget http://leptonica.org/source/leptonica-1.77.0.tar.gz
+tar xvvfz leptonica-1.77.0.tar.gz
+cd leptonica-1.77.0/
+./configure --prefix=/usr/local/
+make
+make install
+cd /usr/src/
+wget https://github.com/tesseract-ocr/tesseract/archive/4.1.0.tar.gz -O tesseract-4.1.0.tar.gz
+tar xvvfz tesseract-4.1.0.tar.gz
+cd tesseract-4.1.0
+wget -nc --quiet https://github.com/tesseract-ocr/tessdata_fast/blob/master/eng.traineddata
+wget -nc --quiet https://github.com/tesseract-ocr/tessdata_fast/blob/master/osd.traineddata
+cp   eng.traineddata osd.traineddata ./tessdata/
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+./autogen.sh
+./configure --prefix=/usr/local/ --with-extra-libraries=/usr/local/lib/
+make install
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 bash miniconda.sh -b
 conda install -c conda-forge tesseract
