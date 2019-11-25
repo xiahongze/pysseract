9fb3a04b1709f750dae275c40e4d9313e4e17feb
StephenHogg <StephenHogg@users.noreply.github.com>
2019-11-25 11:35:07 +1100

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -35,7 +35,7 @@ tar xfz tesseract-4.1.0.tar.gz
 cd tesseract-4.1.0
 mkdir include
 mkdir include/tesseract
-find ./src -name "*.h" -type f | xargs -I {} cp --parents {} ./include/tesseract
+find ./src -name "*.h" -type f | xargs -I {} cp {} ./include/tesseract
 #curl -L https://github.com/tesseract-ocr/tessdata_fast/blob/master/eng.traineddata >> ./eng.traineddata
 #curl -L https://github.com/tesseract-ocr/tessdata_fast/blob/master/osd.traineddata >> ./osd.traineddata
 #cp   eng.traineddata osd.traineddata ./tessdata/
